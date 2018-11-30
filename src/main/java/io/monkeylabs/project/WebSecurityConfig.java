ac4d7ad82c9e5274268b4d87acd82dc5d5a18870
MrK <crodriguezq91@gmail.com>
2018-11-30 13:04:26 -0600

    Base template Spring Security Guide

--- src/main/java/io/monkeylabs/project/WebSecurityConfig.java
+++ src/main/java/io/monkeylabs/project/WebSecurityConfig.java
@@ -1,11 +1,63 @@
 package io.monkeylabs.project;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private static final String ENCODED_PASSWORD = "$2a$10$AIUufK8g6EFhBcumRRV2L.AQNz3Bjp7oDQVFiO5JJMBFZQ6x2/R/2";
+
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+			.authorizeRequests()
+				.antMatchers("/", "/hello").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+	
+	// as with default password encoder is deprecated, we can use an alternative solution	
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user = 
+//				User.withDefaultPasswordEncoder()()
+//					.username("user")
+//					.password("password")
+//					.roles("USER")
+//					.build();
+//		
+//		return new InMemoryUserDetailsManager(user);
+//	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.passwordEncoder(passwordEncoder())
+			.withUser("user").password(ENCODED_PASSWORD).roles("USER"); //password: secret123
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	
+	
 }
